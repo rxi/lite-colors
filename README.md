8cb8e921d5ddd2b728029685b93b1e72157a69f5
rxi <rxi@users.noreply.github.com>
2020-04-26 11:00:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+Color themes for the [lite text editor](https://github.com/rxi/lite)
+
+*Note: if you make a pull request, the relevant table should be updated and kept
+in alphabetical order. The [`make_preview_image.lua`](make_preview_image.lua)
+script should be used to create a preview image for your theme which should be
+embedded in the table.*
+
+---
+
+## Dark
+
+Theme | Preview
+------|-----------------------------------------
+[`monodark`](colors/monodark.lua?raw=1) | ![monodark_preview](https://user-images.githubusercontent.com/3920290/80304201-62353400-87ac-11ea-9b13-9ca1b9db0f99.png)
+[`winter`](colors/winter.lua?raw=1) |![winter_preview](https://user-images.githubusercontent.com/3920290/80304194-5c3f5300-87ac-11ea-9acf-33892579093e.png)
+
+
+## Light
+
+Theme | Preview
+------|-----------------------------------------
+--- | ---
---
+++ make_preview_image.lua
@@ -0,0 +1,61 @@
+#!/usr/bin/lua
+
+local filename = ...
+local name = filename:match("([^\\/]+)%..*$") .. "_preview"
+
+
+-- get colors
+local text = io.open(filename):read("*a")
+local colors = {}
+for r, g, b in text:gmatch("#(%x%x)(%x%x)(%x%x)") do
+  r = tonumber(r, 16)
+  g = tonumber(g, 16)
+  b = tonumber(b, 16)
+  table.insert(colors, { r, g, b })
+end
+
+table.sort(colors, function(a, b)
+  return a[1] + a[2] + a[3] < b[1] + b[2] + b[3]
+end)
+
+local function eq(a, b)
+  return a[1] == b[1] and a[2] == b[2] and a[3] == b[3]
+end
+
+local prev = {}
+for i = #colors, 1, -1 do
+  if eq(colors[i], prev) then
+    table.remove(colors, i)
+  else
+    prev = colors[i]
+  end
+end
+
+
+-- generate ppm file
+local w = 200
+local h = 16
+local fp = io.open(name .. ".ppm", "wb")
+fp:write("P3\n")
+fp:write(w, " ", h, "\n")
+fp:write("255\n")
+
+local row = {}
+for i = 0, w - 1 do
+  local idx = math.floor((#colors / w) * i) + 1
+  local r, g, b = table.unpack(colors[idx])
+  table.insert(row, r)
+  table.insert(row, g)
+  table.insert(row, b)
+end
+row = table.concat(row, " ") .. "\n"
+
+for i = 1, h do
+  fp:write(row)
+end
+fp:close()
+
+
+-- convert ppm file to png
+os.execute(string.format("convert %s.ppm %s.png", name, name))
+os.execute(string.format("rm %s.ppm", name))
