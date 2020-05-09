742ad643afeac4832336f4a2bf0fd7704a60dcd5
Israel Torres <itorres1490@gmail.com>
2020-05-09 16:48:58 -0700

    Added color theme 'dracula'

---
+++ colors/dracula.lua
@@ -0,0 +1,28 @@
+local style = require "core.style"
+local common = require "core.common"
+
+style.background = { common.color "#282a36" }
+style.background2 = { common.color "#282a36" }
+style.background3 = { common.color "#282a36" }
+style.text = { common.color "#f8f8f2" }
+style.caret = { common.color "#f8f8f0" }
+style.accent = { common.color "#8be9fd" }
+style.dim = { common.color "#6272a4" }
+style.divider = { common.color "#6272a4" }
+style.selection = { common.color "#44475a" }
+style.line_number = { common.color "#6272a4" }
+style.line_number2 = { common.color "#6272a4" }
+style.line_highlight = { common.color "#44475a" }
+style.scrollbar = { common.color "#6272a4" }
+style.scrollbar2 = { common.color "#44475a" }
+
+style.syntax["normal"] = { common.color "#f8f8f2" }
+style.syntax["symbol"] = { common.color "#f8f8f2" }
+style.syntax["comment"] = { common.color "#6272a4" }
+style.syntax["keyword"] = { common.color "#ff79c6" }
+style.syntax["keyword2"] = { common.color "#ff79c6" }
+style.syntax["number"] = { common.color "#bd93f9" }
+style.syntax["literal"] = { common.color "#f1fa8c" }
+style.syntax["string"] = { common.color "#f1fa8c" }
+style.syntax["operator"] = { common.color "#ff79c6" }
+style.syntax["function"] = { common.color "#50fa7b" }
