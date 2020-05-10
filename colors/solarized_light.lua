992fb4439e50b926f302b383982d608239b0a220
rxi <rxi@users.noreply.github.com>
2020-05-10 16:49:34 +0100

    Improved divider, scrollbar and background colors on `solarized_light`

--- colors/solarized_light.lua
+++ colors/solarized_light.lua
@@ -3,18 +3,18 @@ local common = require "core.common"
 
 style.background = { common.color "#fdf6e3" }
 style.background2 = { common.color "#eee8d5" }
-style.background3 = { common.color "#fdf6e3" }
+style.background3 = { common.color "#eee8d5" }
 style.text = { common.color "#657b83" }
 style.caret = { common.color "#657b83" }
 style.accent = { common.color "#002b36" }
 style.dim = { common.color "#93a1a1" }
-style.divider = { common.color "#586e75" }
+style.divider = { common.color "#e0dbc8" }
 style.selection = { common.color "#eee8d5" }
 style.line_number = { common.color "#93a1a1" }
 style.line_number2 = { common.color "#002b36" }
 style.line_highlight = { common.color "#eee8d5" }
-style.scrollbar = { common.color "#93a1a1" }
-style.scrollbar2 = { common.color "#586e75" }
+style.scrollbar = { common.color "#e0dbc8" }
+style.scrollbar2 = { common.color "#bfbbaa" }
 
 style.syntax["normal"] = { common.color "#657b83" }
 style.syntax["symbol"] = { common.color "#657b83" }
