02470e0c06a15c5f6c81f59147d1a1429caf3b1f
vallode <vallode@riseup.net>
2021-03-01 23:19:16 +0000

    Add monolight theme

---
+++ colors/monolight.lua
@@ -0,0 +1,29 @@
+local style = require "core.style"
+local common = require "core.common"
+
+style.background = { common.color "#F5F5F5" }
+style.background2 = { common.color "#F5F5F5" }
+style.background3 = { common.color "#F5F5F5" }
+style.text = { common.color "#3C3C3C" }
+style.caret = { common.color "#000000" }
+style.accent = { common.color "#000000" }
+style.dim = { common.color "#B9B9B9" }
+style.divider = { common.color "#D3D3D3" }
+style.selection = { common.color "#D3D3D3" }
+style.line_number2 = { common.color "#000000" }
+style.line_highlight = { common.color "#EDEDED" }
+style.scrollbar = { common.color "#D3D3D3" }
+style.scrollbar2 = { common.color "#D3D3D3" }
+
+style.syntax = {}
+style.syntax["normal"] = { common.color "#3C3C3C" }
+style.syntax["symbol"] = { common.color "#3C3C3C" }
+style.syntax["comment"] = { common.color "#3C3C3C" }
+style.syntax["keyword"] = { common.color "#3C3C3C" }
+style.syntax["keyword2"] = { common.color "#3C3C3C" }
+style.syntax["number"] = { common.color "#3C3C3C" }
+style.syntax["literal"] = { common.color "#3C3C3C" }
+style.syntax["string"] = { common.color "#3C3C3C" }
+style.syntax["operator"] = { common.color "#3C3C3C" }
+style.syntax["function"] = { common.color "#3C3C3C" }
+
