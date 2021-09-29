550d888529dd2f73d3aa0b92f4d47feeca1fbfc6
Ashvith Shetty <ashvithshetty10@gmail.com>
2021-09-29 19:05:32 +0530

    Added theme gruvbox_light

--- README.md
+++ README.md
@@ -33,3 +33,4 @@ Theme | Preview
 [`moe`](colors/moe.lua?raw=1) | ![moe_preview](https://i.imgur.com/IGEtafP.png)
 [`solarized_light`](colors/solarized_light.lua?raw=1) | ![solarized_light_preview](https://user-images.githubusercontent.com/3920290/81503910-233fcc00-92de-11ea-9d6d-0a32212e6f02.png)
 [`solarobj`](colors/solarobj.lua?raw=1) | ![solarobj_preview](https://i.imgur.com/3EHFlx2.png)
+[`gruvbox_light`](#) | ![gruvbox_light_preview](#)
---
+++ colors/gruvbox_light.lua
@@ -0,0 +1,28 @@
+local style = require "core.style"
+local common = require "core.common"
+
+style.background = { common.color "#fbf1c7" }
+style.background2 = { common.color "#f9f5d7" }
+style.background3 = { common.color "#f9f5d7" }
+style.text = { common.color "#928374" }
+style.caret = { common.color "#282828" }
+style.accent = { common.color "#3c3836" }
+style.dim = { common.color "#928374" }
+style.divider = { common.color "#f9f5d7" }
+style.selection = { common.color "#ebdbb2" }
+style.line_number = { common.color "#928374" }
+style.line_number2 = { common.color "#3c3836" }
+style.line_highlight = { common.color "#f2e5bc" }
+style.scrollbar = { common.color "#928374" }
+style.scrollbar2 = { common.color "#282828" }
+
+style.syntax["normal"] = { common.color "#3c3836" }
+style.syntax["symbol"] = { common.color "#3c3836" }
+style.syntax["comment"] = { common.color "#928374" }
+style.syntax["keyword"] = { common.color "#9d0006" }
+style.syntax["keyword2"] = { common.color "#076678" }
+style.syntax["number"] = { common.color "#8f3f71" }
+style.syntax["literal"] = { common.color "#8f3f71" }
+style.syntax["string"] = { common.color "#79740e" }
+style.syntax["operator"] = { common.color "#3c3836" }
+style.syntax["function"] = { common.color "#427b58" }
